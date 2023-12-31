7a86931a61112856fc65985a88f80d79b458ec6d
kimsieun99 <dotoryeop@gmail.com>
2024-01-01 06:19:41 +0900

    1.1완료

---
+++ kimsieun/section2/시작프로젝트/assets/scripts/app.js
@@ -0,0 +1,10 @@
+// // import { apikey } from "./util";
+
+// // import apikey from "./util";
+
+// // import { apikey, abc as content } from "./util";
+
+// import * as util from "./util";
+
+// console.log(util.apikey)
+
---
+++ kimsieun/section2/시작프로젝트/assets/scripts/util.js
@@ -0,0 +1,6 @@
+// export let apikey = "adnasdoasflak1"
+// // 더미 예시
+
+export default "adnasdoasflak1";
+export let apikey = "adnasdoasflak1";
+export let abc = "abc";
