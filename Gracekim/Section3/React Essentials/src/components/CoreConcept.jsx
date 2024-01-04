d10fdf5406a4f89a4fff50ef05ee6dcb2da522f5
gracekim527 <lemonherb0323@naver.com>
2024-01-05 00:38:22 +0900

    :white_check_mark: Feat: add react essential deep dive complete

--- Gracekim/Section3/React Essentials/src/components/CoreConcept.jsx
+++
@@ -1,9 +0,0 @@
-// CORE_CONCEPTS의 데이터를 매개변수로 다시 정의
-export default function CoreConcept({image, title, description}) {
-    return <li>
-      
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  }
