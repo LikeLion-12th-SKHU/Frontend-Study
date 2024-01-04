801b294e8be3dc257b091ce7d237d2698e0d4684
hayoon07 <backhay0714@naver.com>
2024-01-04 14:18:24 +0900

    :sparkles: Feat:~section3.퀴즈1

---
+++ hayun/section3-project/src/App.jsx
@@ -0,0 +1,26 @@
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  ); // 다중 선의 코드 변환시 ()로 묶어 이 코드가 함께 있다는 것을 표시해야 함.
+}
+
+
+function App() { // React Components
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
