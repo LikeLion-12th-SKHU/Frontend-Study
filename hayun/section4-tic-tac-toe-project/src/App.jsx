dff849be2010f44eb39b50d6a26a4da093f0fa96
hayoon07 <backhay0714@naver.com>
2024-01-07 22:50:27 +0900

    :sparkles: Feat: ~section4. 계산된 값 끌어올리기

--- hayun/section4-tic-tac-toe-project/src/App.jsx
+++ hayun/section4-tic-tac-toe-project/src/App.jsx
@@ -1,14 +1,80 @@
+import { useState } from "react";
+
 import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
+import Log from "./components/Log.jsx";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+
+  // 이 부분은 잘못되지는 않았지만 불필요함. - 동일 내용 반복
+  // const [hasWinner, setHasWinner] = useState(false);
+  
+  // const [activePlayer, setActivePlayer] = useState('X');
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+    for (const turn of gameTurns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
+    }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[]
+    const secondSquareSymbol 
+    const thirdSquareSymbol 
+  }
+
+  // 버튼이 클릭되었을 때 실행되는 함수
+  function handleSelectSquare(rowIndex, colIndex) {
+    // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X'); // 플레이어를 바꿈
+    
+    // 이전 상태에 기반하여 상태를 변경하고 싶을 때 밑 코드 사용(setGameTurns)
+    setGameTurns(prevTurns => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col:colIndex }, player: currentPlayer }, 
+        ...prevTurns
+      ];
+
+      return updatedTurns;
+    });
+  } 
+
   return <main>
     <div id="game-container">
-      <ol id="players">
-        <Player name="Player 1" symbol="X" />
-        <Player name="Player 2" symbol="O" />
+      <ol id="players" className="highlight-player">
+        <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+        <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
       </ol>
-      GAME BOARD
+      <GameBoard 
+        onSelectSquare={handleSelectSquare} 
+        board = {gameBoard} 
+      />
     </div>
+    <Log turns={gameTurns} />
   </main>;
 }
 
--- hayun/section4-tic-tac-toe-project/src/components/Player.jsx
+++ hayun/section4-tic-tac-toe-project/src/components/Player.jsx
@@ -1,25 +1,34 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol, isActive}) {
+    const [playerName, setPlayerName] = useState(initialName); // 개별 상태 제어
     const [ isEditing, setIsEditing ] = useState(false);
 
     function handleEditClick() {
-        setIsEditing(true);
+        setIsEditing((editing) => !editing);
+        // setIsEditing(!isEditing); // => schedules a state update to true
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) {
+      setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    // let btnCaption = 'Edit';
 
     if (isEditing) {
-        playerName = <input type="text" required />;
+      // 이 부분을 통해 사용자가 수정하기 위해 입력한 값을 볼 수 있고 저장도 가능해짐. = 양방향 바인딩
+        playerName = <input type="text" required value={playerName} onChange={handleChange} />;
+        // btnCaption = 'Save';
     }
 
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
           <span className="player">
-            {playerName}
+            {editablePlayerName}
             <span className="player-symbol">{symbol}</span>
           </span>
-          <button onClick={handleEditClick}>Edit</button>
+          <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     )
 }
