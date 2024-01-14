96a2a5759e670ff53d6fbed07749c4d9db361ec7
gracekim527 <lemonherb0323@naver.com>
2024-01-15 00:31:35 +0900

    :white_check_mark: Feat: add Section12 - copmlete

--- Gracekim/Section12/ReactQuiz/src/components/QuestionTimer.jsx
+++ Gracekim/Section12/ReactQuiz/src/components/QuestionTimer.jsx
@@ -1,27 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-export default function QuestionTimer({ timeout, onTimeout }) {
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
     console.log('SETTING TIMEOUT');
-    setTimeout(onTimeout, timeout);
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [timeout, onTimeout]);
 
-  // setInterval이 계속해서 렌더링되는 현상을 막기 위한 useEffect()
   useEffect(() => {
     console.log('SETTING INTERVAL');
     const interval = setInterval(() => {
-      setRemainingTime(prevRemainingTime => prevRemainingTime - 100);
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
 
     return () => {
       clearInterval(interval);
     };
-  }, []); // 의존성이 따로 필요없음.
-  
+  }, []);
 
   return (
-    <progress id="question-time" max={timeout} value={remainingTime} />
-  )
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={mode}
+    />
+  );
 }
